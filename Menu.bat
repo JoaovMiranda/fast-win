873b2111b44d308fc389d49e789f7661f3f95857
João Miranda <joaovmirandas@gmail.com>
2020-01-20 18:24:52 -0300

    [UPDATE] Finalizando as opções do novo menu

--- Menu.bat
+++ Menu.bat
@@ -1,15 +1,12 @@
 :: Nome   : Menu.bat
 :: Motivo : Agilizar pequenas ações diárias.
 :: Autor  : github.com/joaovMiranda
-:: VERSÃO : 1.4.1
-
-
+:: VERSÃO : 1.4.3
 @ECHO off
+
 CLS
 TITLE CONTROLE DE ACESSO
 COLOR b
-
-
 rem :control
 rem echo enter password to activate programme
 rem set/p "pass=>"
@@ -39,263 +36,279 @@ PAUSE
 
 REM Criando Pagina Principal 
 :menu
-CLS
-TITLE FACILITADOR DE COMANDOS
-COLOR b
+	CLS
+	TITLE FACILITADOR DE COMANDOS
+	COLOR b
 REM Acessando Dados do Computador
 
 ::MODE 40,35
 
-ECHO    USUARIO: %username% 
-ECHO    COMPUTADOR: %computername% 
-ECHO    DATA: %date%   %time%
+	ECHO    USUARIO: %username% 
+	ECHO    COMPUTADOR: %computername% 
+	ECHO    DATA: %date%   %time%
 
-ECHO    (c) 2019 Microsoft Corporation. Todos os direitos reservados.                                                                   
-                                                              
-                                                                                                                                    
-ECHO          ----- MENU TAREFAS -----
-ECHO     ==================================
-ECHO    * 3.  Escanear Disco Local         *
-ECHO    * 4.  Informacoes da Maquina       *
-
-ECHO    * .  BACKUP                        * 
-ECHO    * .  EXECUTAVEIS                   *
-ECHO    * .  REDE                          *
-ECHO    * .  Reiniciar o Computador        *
-ECHO    * .  Desligar o Computador         *
-
-ECHO    * 10. Esvaziar a Lixeira           *
-ECHO    * 11. Desfragmentar o Disco        *
-ECHO    * 12. Limpar Fila de Impressao     *
-ECHO    * 13. Limpar Arquivos Temporarios  *
-ECHO    * 14. Limpeza Interna              *
-
-ECHO     ==================================
-ECHO    * 65. Voltar ao Guia               *
-ECHO    * 50. Sair                         *
-ECHO     ==================================
+	ECHO    (c) 2019 Microsoft Corporation. Todos os direitos reservados.                                                                   
+																
+																																		
+	ECHO          ----- MENU TAREFAS -----
+	ECHO     ==================================
+	ECHO    * 1.  Informacoes da Maquina       *
+	ECHO    * 2.  Opcoes de Limpeza            * 
+	ECHO    * 3.  Opcoes de DISCO              * 
+	ECHO    * 4.  Opcoes de Backup             * 
+	ECHO    * 5.  Opcoes de Rede               *
+	ECHO    * 6.  Abrir Executaveis            *
+	ECHO    * 7.  Reiniciar o Computador       *
+	ECHO    * 8.  Desligar o Computador        *
+	ECHO     ==================================
+	ECHO    * 10. GitHub                       *
+	ECHO    * 20. Voltar ao Guia               *
+	ECHO    * 50. Sair                         *
+	ECHO     ==================================
 
 
+	REM Estrutura de Afirmações Para Menu
+	SET /p opcao= Escolha uma opcao: 
+	ECHO ------------------------------
+	IF %opcao% EQU 1 GOTO INFO
+	IF %opcao% EQU 2 GOTO LIMP
+	IF %opcao% EQU 3 GOTO DISCO
+	IF %opcao% EQU 4 GOTO BACKUP
+	IF %opcao% EQU 5 GOTO REDE
+	IF %opcao% EQU 6 GOTO EXE
+	IF %opcao% EQU 7 GOTO REINICIAR
+	IF %opcao% EQU 7 GOTO DESLIGAR
+	IF %opcao% EQU 20 GOTO wellcome
+	IF %opcao% EQU 10 GOTO github
+	IF %opcao% EQU 50 GOTO EXIT
 
-REM Estrutura de Afirmações Para Menu
-SET /p opcao= Escolha uma opcao: 
-ECHO ------------------------------
-IF %opcao% EQU 1 GOTO opcao1
-IF %opcao% EQU 2 GOTO opcao2
-IF %opcao% EQU 3 GOTO opcao3
-IF %opcao% EQU 4 GOTO opcao4
-IF %opcao% EQU 5 GOTO opcao5
-IF %opcao% EQU 6 GOTO opcao6
-IF %opcao% EQU 7 GOTO opcao7
-
-::IF %opcao% EQU 8 GOTO EXE
-::IF %opcao% EQU 9 GOTO REDE
-
-
-IF %opcao% EQU 10 GOTO opcao10
-IF %opcao% EQU 11 GOTO opcao11
-IF %opcao% EQU 12 GOTO opcao12
-IF %opcao% EQU 13 GOTO opcao13
-IF %opcao% EQU 14 GOTO opcao14
-IF %opcao% EQU 15 GOTO opcao15
-IF %opcao% EQU 16 GOTO opcao16
-
-IF %opcao% EQU 65 GOTO wellcome
-IF %opcao% EQU 75 GOTO github
-IF %opcao% EQU 70 GOTO opcao70
-IF %opcao% EQU 85 GOTO opcao85
-
-
-REM Sair do programa
-IF %opcao% EQU 50 GOTO opcao50
-
-REM Se menor que 1 ou igual maior que X : Opção inválida
-IF %opcao% LSS 1 GOTO opcao100
-IF %opcao% GEQ 17 GOTO opcao100
-
-ECHO copyright
+	REM Se menor que 1 ou igual maior que X : Opção inválida
+	IF %opcao% LSS 1 GOTO ERROR
 
 
 REM Informações sobre a máquina
-:opcao4
-CLS
-TITLE INFORMACOES
-COLOR c
-ECHO 				 ==================================
-ECHO 				*           SUA PLACA MAE          *
-ECHO 				 ==================================
-wmic baseboard get product, manufacturer, version, serialnumber
-ECHO   				 ==================================
-ECHO  				*      INFORMACOES DO SISTEMA      *
-ECHO  				 ==================================
-systeminfo
-systeminfo > c:\INFO.txt
-ECHO   				 ==================================
-ECHO   				 Arquivo INFO.txt gerado no disco C:
-ECHO   				 ==================================
-PAUSE
-GOTO menu
-
-
-:opcao50
+:INFO
 	CLS
-	exit
-
-:opcao100
-	ECHO  ======================================
-	ECHO * Opcao Invalida! Escolha outra opcao. *
-	ECHO  ======================================
-	PAUSE
-	GOTO menu
-
-:fail
-	ECHO Senha incorreta, tente novamente.
-	PAUSE
-	GOTO control
-
-
-:github
-	START https://github.com/joaovMiranda
-	CLS
-	GOTO menu
-
-
-:LIMPEZA
-	CLS
-	REM Apaga Todos os Arquivos da Lixeira
-	:opcao10
-	CLS
-	TITLE LIMPANDO ...
-	RD /S /Q c:\$Recycle.bin
-	ECHO  ==================================
-	ECHO *      Lixeira Esvaziada           *
-	ECHO  ==================================
-	PAUSE
-	GOTO menu
-	REM Apagando arquivos desnecessarios
-	:opcao14
-	CLS
-	DEL C:\Windows\System32\CLINT.*.*  /q
-	DEL C:\Windows\System32\LOAD.*.*   /q
-	DEL C:\Windows\System32\GIF.*.* /q
-	DEL c:\windows\spool\printers   /q
-	DEL /s C:\windows\temp\*.* /q 
-	DEL /F /S /Q C:\WINDOWS\Temp\*.*
-	DEL "%WINDIR%\Temp\*.*" /F /S /Q
-	RD /S /Q "%HOMEPATH%\Config~1\Temp"
-	MD "%HOMEPATH%\Config~1\Temp"
-	RD /S /Q C:\WINDOWS\Temp\
-	MD C:\WINDOWS\Temp
-	DEL /F /S /Q %HOMEPATH%\Config~1\Temp\*.*
-	DEL %temp% /q
-	RD /S /Q C:\RECYCLER\ 
-	DEL /s   C:\windows\system32\dllcache   /q
-	DEL /s   C:\MSOCache\*.*   /q
-	SC stop DiagTrack
-	SC stop dmwappushservice
-	SC Delete DiagTrack
-	SC Delete dmwappushservice
-	PAUSE
-	GOTO menu
-	REM Limpar Arquivos Temporarios
-	:opcao13
-	IF EXIST c:\windows\temp\ (
-	forfiles /p C:\Windows\Temp /s /m *.* /D -7 /C “cmd /c del /Q @path”
-	)
-	IF EXIST C:\Users\ (
-	for /D %%x in (“C:\Users\*”) do (
-	forfiles /p %%x\AppData\Local\Temp /s /m *.* /D -7 /C “cmd /c del /Q @path”
-	forfiles /p %%x\AppData\Local\Microsoft\Windows\Temporary Internet Files /s /m *.* /D -7 /C “cmd /c del /Q @path”
-	forfiles /p %%x\AppData\Local\Microsoft\Windows\WER\ReportQueue /s /m *.* /C “cmd /c del /Q @path”
-	)
-	)
-	PAUSE
-	GOTO menu
-	:opcao12
-	REM Limpar fila de impressão 
-	CLS
-	NET STOP spooler
-	c:
-	CD %systemroot%\system32\spool\printers
-	DEL /f/s *.shd
-	DEL /f/s *.spl
-	NET START spooler
-	ECHO  =======================================
-	ECHO +          Processo Finalizado          +
-	ECHO  =======================================
-	PAUSE
-	GOTO menu
-
-:DISCO
-	REM Desfragmantar o disco 
-	:opcao11
-	CLS
-	TITLE OTIMIZANDO ...
-	ECHO  ==================================
-	ECHO *     Desfragmentar Disco via:     *
-	ECHO *                                  *
-	ECHO *       1. Prompt                  *
-	ECHO *       0. Exec. nativo            *
-	ECHO  ==================================
-	SET /p dfrag= Digite :
-	IF %dfrag% EQU 1 GOTO oppp1
-	IF %dfrag% GTR 1 GOTO oppp3
-	IF %dfrag% EQU 0 GOTO oppp2
-	IF %dfrag% LSS 0 GOTO oppp3
-		:oppp1
-		DEFRAG C: /U /V
-		:oppp2
-		START dfrgui.exe
-		GOTO menu
-		:oppp3
-		ECHO  ============================================
-		ECHO * Opcao Invalida! Escolha outra opcao. (1/0) *
-		ECHO  ============================================
-		PAUSE
-	
-	PAUSE
-	GOTO menu 
-
-	:opcao85
-	CLS
-	SFC /scannow
-	PAUSE
-	GOTO menu
-
-
-	REM CHECKDISK 
-	:opcao3
-	CLS
-	TITLE ESCANEANDO ...
-	ECHO  				 ==================================
-	ECHO  				*       Escaneamento de Disco       *
+	TITLE INFORMACOES
+	COLOR c
+	ECHO 				 ==================================
+	ECHO 				*           SUA PLACA MAE          *
+	ECHO 				 ==================================
+	wmic baseboard get product, manufacturer, version, serialnumber
 	ECHO   				 ==================================
-	chkdsk c:
+	ECHO  				*      INFORMACOES DO SISTEMA      *
+	ECHO  				 ==================================
+	systeminfo
+	systeminfo > c:\INFO.txt
+	ECHO   				 ==================================
+	ECHO   				 Arquivo INFO.txt gerado no disco C:
+	ECHO   				 ==================================
 	PAUSE
 	GOTO menu
 
-::TUDO OK DAQUI PRA BAIXO
+:LIMP
+	CLS
+	TITLE LIMPEZA
+	ECHO     ------------------------------
+	ECHO    * 1. Esvaziar a Lixeira         *
+	ECHO    * 2. Limpeza Interna            *
+	ECHO    * 3. Limpar Arquivos Temporarios*
+	ECHO    * 4. Limpar Fila de Impressao   *
+	ECHO    * 5. Voltar                     *
+	ECHO     ------------------------------
+	SET /p limp= Selecione :
 
+	IF %limp% EQU 1 GOTO limp1
+	IF %limp% EQU 2 GOTO limp2
+	IF %limp% EQU 3 GOTO limp3
+	IF %limp% EQU 4 GOTO limp4
+	IF %limp% EQU 5 GOTO limp5
+
+	IF %limp% GTR 5 GOTO limp6
+	IF %limp% LSS 1 GOTO limp6
+
+REM Apaga Todos os Arquivos da Lixeira
+	:limp1
+		CLS
+		TITLE LIMPANDO ...
+		RD /S /Q c:\$Recycle.bin
+		ECHO  ==================================
+		ECHO *      Lixeira Esvaziada           *
+		ECHO  ==================================
+		PAUSE
+		GOTO menu
+
+REM Apagando arquivos desnecessarios
+	:limp2
+		TITLE LIMPANDO ARQUIVOS ...
+		CLS
+		DEL C:\Windows\System32\CLINT.*.*  /q
+		DEL C:\Windows\System32\LOAD.*.*   /q
+		DEL C:\Windows\System32\GIF.*.* /q
+		DEL c:\windows\spool\printers   /q
+		DEL /s C:\windows\temp\*.* /q 
+		DEL /F /S /Q C:\WINDOWS\Temp\*.*
+		DEL "%WINDIR%\Temp\*.*" /F /S /Q
+		RD /S /Q "%HOMEPATH%\Config~1\Temp"
+		MD "%HOMEPATH%\Config~1\Temp"
+		RD /S /Q C:\WINDOWS\Temp\
+		MD C:\WINDOWS\Temp
+		DEL /F /S /Q %HOMEPATH%\Config~1\Temp\*.*
+		DEL %temp% /q
+		RD /S /Q C:\RECYCLER\ 
+		DEL /s   C:\windows\system32\dllcache   /q
+		DEL /s   C:\MSOCache\*.*   /q
+		SC stop DiagTrack
+		SC stop dmwappushservice
+		SC Delete DiagTrack
+		SC Delete dmwappushservice
+		PAUSE
+		GOTO menu
+
+REM Limpar Arquivos Temporarios
+	:limp3
+		TITLE LIMPANDO ARQUIVOS TEMP...
+		IF EXIST c:\windows\temp\ (
+		forfiles /p C:\Windows\Temp /s /m *.* /D -7 /C “cmd /c del /Q @path”
+		)
+		IF EXIST C:\Users\ (
+		for /D %%x in (“C:\Users\*”) do (
+		forfiles /p %%x\AppData\Local\Temp /s /m *.* /D -7 /C “cmd /c del /Q @path”
+		forfiles /p %%x\AppData\Local\Microsoft\Windows\Temporary Internet Files /s /m *.* /D -7 /C “cmd /c del /Q @path”
+		forfiles /p %%x\AppData\Local\Microsoft\Windows\WER\ReportQueue /s /m *.* /C “cmd /c del /Q @path”
+		)
+		)
+	PAUSE
+	GOTO menu
+
+REM Limpar fila de impressão 	
+	:limp4
+		CLS
+		NET STOP spooler
+		c:
+		CD %systemroot%\system32\spool\printers
+		DEL /f/s *.shd
+		DEL /f/s *.spl
+		NET START spooler
+		ECHO  =======================================
+		ECHO +          Processo Finalizado          +
+		ECHO  =======================================
+		PAUSE
+		GOTO menu
+
+	:limp5
+		CLS
+		GOTO menu
+
+	:limp6
+		CLS
+		ECHO 	=======================================
+		ECHO  * Opcao Invalida! Escolha outra opcao.    *
+		ECHO 	=======================================
+		PAUSE
+		GOTO LIMP
+
+REM Opções de Disco
+:DISCO
+	CLS
+	TITLE LIMPEZA
+	ECHO     ------------------------------
+	ECHO    * 1. Desfragmantar disco (C:)   *
+	ECHO    * 2. Verificar Integridade      *
+	ECHO    * 3. Checar saúde do Disco      *
+	ECHO    * 4. Voltar                     *
+	ECHO     ------------------------------
+	SET /p disc= Selecione :
+
+	IF %disc% EQU 1 GOTO disc1
+	IF %disc% EQU 2 GOTO disc2
+	IF %disc% EQU 3 GOTO disc3
+	IF %disc% EQU 4 GOTO disc4
+
+	IF %disc% GTR 4 GOTO disc5
+	IF %disc% LSS 1 GOTO disc5
+
+REM Desfragmantar o disco
+	:disc1
+		CLS
+		TITLE OTIMIZANDO ...
+		ECHO  ==================================
+		ECHO *     Desfragmentar Disco via:     *
+		ECHO *                                  *
+		ECHO *       1. Prompt                  *
+		ECHO *       0. Exec. nativo            *
+		ECHO  ==================================
+		SET /p dfrag= Digite :
+		IF %dfrag% EQU 1 GOTO oppp1
+		IF %dfrag% EQU 0 GOTO oppp2
+
+		IF %dfrag% GTR 1 GOTO oppp3
+		IF %dfrag% LSS 0 GOTO oppp3
+
+			:oppp1
+				DEFRAG C: /U /V
+				PAUSE
+				GOTO menu
+			:oppp2
+				START dfrgui.exe
+				GOTO menu
+			:oppp3
+				ECHO  ============================================
+				ECHO * Opcao Invalida! Escolha outra opcao. (1/0) *
+				ECHO  ============================================
+				PAUSE
+				GOTO disc1 
+
+REM Escanear integridade do sistema
+	:disc2
+		CLS
+		SFC /scannow
+		PAUSE
+		GOTO menu
+
+REM CHECKDISK 
+	:disc3
+		CLS
+		TITLE ESCANEANDO ...
+		ECHO  				 ==================================
+		ECHO  				*       Escaneamento de Disco       *
+		ECHO   				 ==================================
+		chkdsk c:
+		PAUSE
+		GOTO menu
+
+	:disc4
+		CLS
+		GOTO menu
+
+	:disc5
+		CLS
+		ECHO 	=======================================
+		ECHO  * Opcao Invalida! Escolha outra opcao.    *
+		ECHO 	=======================================
+		PAUSE
+		GOTO DISCO
+
+REM Tipos de BackUP
 :BACKUP
 	CLS
-	REM Tipos de BackUP
-	TITLE BACK UP
-	ECHO  ------------------------------
+	TITLE BACK-UP
+	ECHO     ---------------------------------
 	ECHO    * 1.  Fazer Backup dos Documentos  * 
 	ECHO    * 2.  Limpar Area de Trabalho      *
 	ECHO    * 3.  Voltar                       *
-	ECHO  ------------------------------
+	ECHO     ---------------------------------
 	SET /p bac= Selecione :
 
 	IF %bac% EQU 1 GOTO bac1
 	IF %bac% EQU 2 GOTO bac2
 	IF %bac% EQU 3 GOTO bac3
 
-	IF %bac% GTR 5 GOTO bac6
+	IF %bac% GTR 3 GOTO bac6
 	IF %bac% LSS 1 GOTO bac6
 
+REM Copia Arquivos da Pasta 'Documentos' Para Uma Pasta de Backup; Insira Seu Endereço de Escolha
 	:bac1
-		REM Copia Arquivos da Pasta 'Documentos' Para Uma Pasta de Backup; Insira Seu Endereço de Escolha
 		CLS
 		TITLE BACK UP - Documents
 		ECHO  				 ==================================
@@ -307,9 +320,9 @@ GOTO menu
 		ECHO  				 ==================================
 		PAUSE
 		GOTO menu
-	
+
+REM MOVE Todos os Arquivos do Desktop Para Uma Pasta de Backup
 	:bac2
-		REM MOVE Todos os Arquivos do Desktop Para Uma Pasta de Backup
 		CLS
 		TITLE BACK UP - Desktop
 		ECHO 				 ==================================
@@ -340,18 +353,17 @@ GOTO menu
 		PAUSE
 		GOTO EXE
 
-
 :EXE
+REM Iniciar executáveis 
 	CLS
-	REM Iniciar executáveis 
 	TITLE EXECUTAVEIS
-	ECHO  ------------------------------
+	ECHO      ------------------------------
 	ECHO    * 1.  Abrir Calculadora            *
 	ECHO    * 2.  Abrir Painel de Controle     *
 	ECHO    * 3.  Abrir Ger. Tarefas           *
 	ECHO    * 4.  Desinstalar Programas        *
 	ECHO    * 5.  Voltar                       *
-	ECHO  ------------------------------
+	ECHO      ------------------------------
 	SET /p exec= Selecione :
 
 	IF %exec% EQU 1 GOTO exec1
@@ -394,7 +406,6 @@ GOTO menu
 		PAUSE
 		GOTO EXE
 
-
 :REDE
 	CLS
 	TITLE REDE 
@@ -404,7 +415,6 @@ GOTO menu
 	ECHO *   3. Voltar                  *
 	ECHO  ------------------------------
 	SET /p red= Selecione :
-
 	IF %red% EQU 1 GOTO oppp1
 	IF %red% EQU 2 GOTO oppp2
 	IF %red% EQU 3 GOTO oppp3
@@ -412,8 +422,8 @@ GOTO menu
 	IF %red% GTR 3 GOTO oppp4
 	IF %red% LSS 1 GOTO oppp4
 
+REM Teste de instabilidade 
 	:oppp1
-		REM Teste de instabilidade 
 		CLS
 		TITLE TESTANDO ...
 		ECHO  =======================================
@@ -431,8 +441,9 @@ GOTO menu
 		ECHO  =======================================
 		PAUSE
 		GOTO menu
+
+REM  Melhorando sua internet
 	:oppp2
-		REM  Melhorando sua internet
 		CLS
 		TITLE TRABALHANDO ...
 		ipconfig /flushdns
@@ -443,9 +454,11 @@ GOTO menu
 		ECHO  =======================================
 		PAUSE
 		GOTO menu
+
 	:oppp3
 		CLS
 		GOTO menu
+
 	:oppp4
 		ECHO 	=======================================
 		ECHO * Opcao Invalida! Escolha outra opcao.    *
@@ -468,15 +481,15 @@ GOTO menu
 	IF %rein% EQU 0 GOTO op2
 	IF %rein% LSS 0 GOTO op3
 		:op1
-		shutdown -r -c "O Seu Computador Sera Reiniciado" -t "5"
+			shutdown -r -c "O Seu Computador Sera Reiniciado" -t "5"
 		:op2
-		GOTO menu
+			GOTO menu
 		:op3
-		ECHO 	=======================================
-		ECHO * Opcao Invalida! Escolha outra opcao. (1/0) *
-		ECHO 	=======================================
-		PAUSE
-		GOTO opcao15
+			ECHO 	=======================================
+			ECHO * Opcao Invalida! Escolha outra opcao. (1/0) *
+			ECHO 	=======================================
+			PAUSE
+			GOTO opcao15
 
 :DESLIGAR
 	CLS
@@ -501,6 +514,33 @@ GOTO menu
 			ECHO 	=======================================
 			PAUSE
 			GOTO opcao16
+
+:EXIT
+	CLS
+	exit
+
+:ERROR
+	ECHO  ======================================
+	ECHO * Opcao Invalida! Escolha outra opcao. *
+	ECHO  ======================================
+	PAUSE
+	GOTO menu
+
+:fail
+	ECHO Senha incorreta, tente novamente.
+	PAUSE
+	GOTO control
+
+:github
+	START https://github.com/joaovMiranda/Menu_Agil
+	CLS
+	GOTO menu
+
+
+
+
+
+
 
 
 
